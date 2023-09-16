5087a594f231a2105c16ce992937f240c069200f
Njunju95 <nhlaponomvula07@gmail.com>
2023-09-15 17:46:32 -0700

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,13 +7,3 @@ def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
         del my_list[idx]
     return (my_list)
-
-================================
-
-12-switch.py 
-
-#!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
